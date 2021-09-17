e7055682a05507a1cb9c833a947412c9c63ac24f
archi-Doc <archidoc422@gmail.com>
2021-09-17 22:06:10 +0900

    .

---
+++ ModuleInitializerGenerator/TinyGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace ModuleInitializerGenerator
+{
+    [Generator]
+    public class TinyGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context) { }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var text = @"namespace ModuleInitializerGenerator
+{
+    public static class TinyClass
+    {
+        [System.Runtime.CompilerServices.ModuleInitializerAttribute]
+        public static void Initialize()
+        {
+        }
+    }
+}";
+            context.AddSource($"Generator_ModuleInitializer_gen.cs", text);
+        }
+    }
+}
